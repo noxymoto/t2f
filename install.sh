261220aaadeb0599dae78e3e9b6caeab48d78f3f
noxymoto <braceletjunkie@proton.me>
2026-03-30 21:12:06 -0400

    Add installation script for t2f compiler

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+URL="https://github.com/noxymoto/t2f/releases/download/linux-x64-pret2f-compiler/t2f"
+
+echo "Downloading t2f compiler..."
+curl -fsSL "$URL" -o /tmp/t2f
+
+sudo cp /tmp/t2f /usr/local/bin/t2f
+sudo chmod +x /usr/local/bin/t2f
+
+rm /tmp/t2f
+
+echo "t2f installed to /usr/local/bin/t2f"
